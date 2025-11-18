42469f4345036936f950017963255f609e5191a8
Veer Patta <veerpattaschool@gmail.com>
2025-11-18 12:17:42 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,7 +4,7 @@ import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_oilbMzJkGq__9QIs-P6wKVr6hTweCc",
+  apiKey: "AIzaSyC_oilbKMzJKGq__9QIs-P6wKVr6hTweCc",
   authDomain: "veer-patta-fees-system.firebaseapp.com",
   projectId: "veer-patta-fees-system",
   storageBucket: "veer-patta-fees-system.firebasestorage.app",
